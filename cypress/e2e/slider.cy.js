c6bf9dfa9c4d9b6054eeab338fd10abd26a7ebc7
pp <piotrplaneta123@gmail.com>
2025-03-31 14:34:07 +0200

    Added additional cypress test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,34 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if user can navigate slides using next and previous buttons', function () {
+    cy.visit('http://localhost:3000'); 
+    
+    // Sprawdzenie, czy pierwszy slajd to "Rome"
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+
+    // Kliknięcie "Next" i sprawdzenie, czy aktywny slajd to "London"
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'London');
+
+    // Kliknięcie "Next" ponownie i sprawdzenie, czy aktywny slajd to "Paris"
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+
+    // Kliknięcie "Previous" i sprawdzenie, czy aktywny slajd to "London"
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'London');
+
+    // Kliknięcie "Previous" ponownie i sprawdzenie, czy wracamy do "Rome"
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+
